0458b3e3be0077d3e31bc079e910d40b2bc06c11
ANISH-KARTHIC <24uad110anish@kgkite.ac.in>
2025-08-28 12:57:29 +0530

    Update m6_1.py

--- m6.1/m6_1.py
+++ m6.1/m6_1.py
@@ -1,2 +1,64 @@
 def binary_search_guessing_game():
     #Your creative code goes here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#do not edit below this line
+if _name_ == "_main_":
+    binary_search_guessing_game()
+    
